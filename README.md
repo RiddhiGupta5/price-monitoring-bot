6820ea5c64ac76c9a9ac96f5e97829b79873d683
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-04-26 14:08:04 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# price-monitoring-bot
-A bot that will help you monitor price of products on a website
+# Price Monitoring Bot
+A bot that will help you monitor price of products on a website and notify you using a telegram bot. 
+
+## Instructions to run
+
+* Pre-requisites:
+	-  Python3
+
+* Directions to install
+	- Setting up a virtual env 
+	```bash
+	virtualenv env
+	env\Scripts\activate
+	```
+	- Installing Packages
+	```bash
+	pip install -r requirements.txt
+	```
+
+## Instructions to setup own Telegram bot
+
+* Go to Telegram bot_father
+* Create new Bot using /newbot
+* Give your bot name and copy the apikey
+* Go to your bot on telegram, click on start and send a message to it
+* In browser go to https://api.telegram.org/bot{apikey}/getUpdates and copy the chat id
+* Use https://api.telegram.org/bot{apikey}/sendMessage?text={message}&chat_id={chat_id} to send messages to your telegram bot
---
+++ price_monitoring_bot.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def send_message(product_name, product_price):
+    message = "Product Found\n" + product_name + '\n' + product_price
+    url = "https://api.telegram.org/bot890324614:AAGm5leUftjHfnI6DHFzxHlvHoutFAZSvqc/sendMessage?text={}&chat_id={}".format(message, 689114107)
+    requests.get(url)
+
+def check_price():
+    url = "https://www.jabong.com/find/kurtas?tt=kurta&rank=0"
+    response = requests.get(url)
+    print(response)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    main_body = soup.find('section', class_='row search-product animate-products')
+    all_products = main_body.find_all('div', class_='col-xxs-6 col-xs-4 col-sm-4 col-md-3 col-lg-3 product-tile img-responsive')
+    for product in all_products:
+        product_info = product.find('div', class_='product-info')
+        product_name = product_info.find('div', class_='h4')
+        product_price = product_info.find('div', class_='price')
+        print(product_name.text)
+        print(product_price.text)
+        if float(product_price.text) < 1000:
+            send_message(product_name.text, product_price.text)
+
+while(True):
+    check_price()
+    time.delay(60*60*12)
